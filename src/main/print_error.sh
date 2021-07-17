31d2b6da79fbb67a39726e1d6ac87ca0a6d7d619
Action <dev@action-server.com>
2021-07-17 21:05:14 +0200

    init

---
+++ src/main/print_error.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Author:       Action <dev@action-server.com>
+# License:      GNU GPLv3
+# Description:  A unified error function
+
+# ENV
+NAME="$(basename "$0")"
+
+print_error(){
+	message="$1"
+	echo "${NAME} - Error: ${message}" >&2
+}
+
+# Usage
+# print_error 'Something went wrong'
